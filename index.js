e2cde1d9e149cf16788965dddb22b5b0d16a54b4
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-04-06 10:56:44 +0600

    ESlint added

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const Person = require('./models/person');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
-morgan.token("post-body", (req, res) => JSON.stringify(req.body));
+morgan.token('post-body', (req, res) => JSON.stringify(req.body))
 
-app.use(express.json());
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post-body"));
-app.use(cors());
-app.use(express.static('dist'));
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
+app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
+  res.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({}).then(persons => {
-        res.json(persons);
-    }).catch(err => next(err));
-});
+  Person.find({}).then(persons => {
+    res.json(persons)
+  }).catch(err => next(err))
+})
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person);
-    }).catch(err => next(err));
-});
+  Person.findById(req.params.id).then(person => {
+    res.json(person)
+  }).catch(err => next(err))
+})
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body;
+  const body = req.body
 
-    if (!body.name || !body.number) {
-        return res.status(400).json({ error: 'Name or number is missing' });
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'Name or number is missing' })
+  }
+
+  Person.findOne({ name: body.name }).then(existingPerson => {
+    if (existingPerson) {
+      return res.status(400).json({ error: 'Name already exists in the phonebook' })
     }
 
-    Person.findOne({ name: body.name }).then(existingPerson => {
-        if (existingPerson) {
-            return res.status(400).json({ error: 'Name already exists in the phonebook' });
-        }
+    const person = new Person({
+      name: body.name,
+      number: body.number
+    })
 
-        const person = new Person({
-            name: body.name,
-            number: body.number
-        });
-
-        person.save().then(savedPerson => {
-            res.json(savedPerson);
-        }).catch(err => next(err));
-    });
-});
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    }).catch(err => next(err))
+  })
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id;
-    Person.findByIdAndDelete(id).then(() => {
-        res.status(204).end();
-    }).catch(err => next(err));
-});
+  const id = req.params.id
+  Person.findByIdAndDelete(id).then(() => {
+    res.status(204).end()
+  }).catch(err => next(err))
+})
 
 app.get('/info', (req, res, next) => {
-    Person.countDocuments({}).then(count => {
-        res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`);
-    }).catch(err => next(err));
-});
+  Person.countDocuments({}).then(count => {
+    res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
+  }).catch(err => next(err))
+})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,25 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Person = require('./models/person');
-const name = process.argv[2];
-const number = process.argv[3];
+const Person = require('./models/person')
+const name = process.argv[2]
+const number = process.argv[3]
 
-if (process.argv.length == 3) {
-    console.log("phonebook:");
-    Person.find({}).then(res => {
-        res.forEach(person => {
-            console.log(`${person.name} ${person.number}`);
-        })
-        mongoose.connection.close();
+if (process.argv.length === 3) {
+  console.log('phonebook:')
+  Person.find({}).then(res => {
+    res.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 } else {
-    const person = new Person({
-        name: name,
-        number: number
-    });
-    
-    person.save().then(res => {
-        console.log(`Added ${person.name} number ${person.number} to the phonebook`);
-        mongoose.connection.close();
-    });    
+  const person = new Person({
+    name,
+    number
+  })
+  person.save().then(({ name, number }) => {
+    console.log(`Added ${name} number ${number} to the phonebook`)
+    mongoose.connection.close()
+  })
 }
